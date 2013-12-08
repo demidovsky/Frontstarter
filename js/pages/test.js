148324ef1352ca26298dcc9421c5113be42b04d0
Dmitry Demidovsky <d.demidovsky@gmail.com>
2013-12-08 04:58:38 +0400

    Изменено подключение модулей для тестовой страницы

--- js/pages/test.js
+++ js/pages/test.js
@@ -1,125 +1,53 @@
-console.log("test.js loaded");
+/*
+	Скрипты, необходимые на странице test.
+
+	Плейсхолдеры
+	Селекты
+	Фикс для border-radius
+	Календарь
+	Валидация
+
+*/
+
+console.log("loaded: test.js");
 
 $(function()
 {
 
 
 
-	/* кастомные плейсхолдеры */
-	(function CustomizePlaceholders()
-	{
-		var $placeholder = $('.js-placeholder');
-
-		yepnope({
-			test: $placeholder.length,
-			yep: window.ROOT + "js/modules/placeholder.js",
-			complete: function()
-			{
-				$placeholder.placeholder();
-				console.log("placeholders done.");
-			}
-		});
-	})();
-
-
-
-
-	/* кастомные выпадающие списки */
-	(function CustomizeSelects()
-	{
-		var $combobox = $('.js-ufd');
-
-		yepnope({
-			test: $combobox.length,
-			yep: [window.ROOT + "js/modules/jquery-ui-1.10.3.custom.min.js",
-				  window.ROOT + "js/modules/jquery.ui.ufd.min.js",
-				  window.ROOT + "css/modules/ufd.css"],
-			complete: function()
-			{
-				// здесь обращение к стандартному селекту
-				$combobox.ufd();
-				// а здесь - уже к кастомному
-				$('.ufd').children('input').on('focus', function() { $(this).blur(); });
-				console.log("ufd done.");
-			}
-		});
-	})();
-
-
-
-
 	/* polyfill для кругляшей */
 	(function FixBorderRadius()
 	{
-		yepnope({
-			test: Modernizr.borderradius,
-			nope: window.ROOT + "js/modules/jquery.corner.js",
-			callback: function(testResult)
-			{
-				if (!testResult) return;
-				$('.js-rounded').corner("20px");
-				console.log("corner.js applied.");
-			}
-		});
-	})();
+		var $rounded = $('.js-rounded');
 
-
-
-
-	(function Datepicker()
-	{
-		var $datepicker = $('.js-datepicker');
-		var $open = $('.js-datepicker-open');
-
-		yepnope({
-			test: $datepicker.length,
-			yep: [window.ROOT +  "js/modules/jquery-ui-1.10.3.custom.min.js",
-				  window.ROOT + "css/modules/jquery-ui-1.10.3.custom.min.css"],
-			complete: function()
-			{
-				$.datepicker.regional['ru'] =
+		if ($rounded.length && !Modernizr.borderradius)
+		{
+			yepnope({
+				load: window.ROOT + "js/modules/jquery.corner.js",
+				complete: function(testResult)
 				{
-					closeText: 'Закрыть',
-					prevText: '&#x3c;Пред',
-					nextText: 'След&#x3e;',
-					currentText: 'Сегодня',
-					monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-					'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-					monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
-					'Июл','Авг','Сен','Окт','Ноя','Дек'],
-					dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-					dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-					dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-					dateFormat: 'dd.mm.yy',
-					firstDay: 1,
-					isRTL: false
-				}; 
+					//if (!testResult) return;
+					console.log(0);
+					
+					$rounded.corner("20px");
 
-				$.datepicker.setDefaults($.datepicker.regional["ru"]);
-
-				$datepicker.datepicker();
-
-				$open.click(function(event)
-				{
-					event.preventDefault();
-					$(this).siblings('.js-datepicker').datepicker("show");
-				});
-
-				console.log("datepicker done.");
-			}
-		});
+					console.log("finished: FixBorderRadius");
+				}
+			});
+		}
 	})();
 
 
 
 
-
-	/* Новый класс */
-	(function NewClassName()
+	/* Работа с формами */
+	(function Forms()
 	{
-		// Да будет код!
-	})();
+		yepnope(window.ROOT + "js/modules/forms.js");
 
+		console.log("finished: Forms");
+	})();
 
 
 
