e139b8e418ad390672c2fbd4e24841c3e14a7a49
Dmitry Demidovsky <d.demidovsky@gmail.com>
2014-08-22 19:52:12 +0400

    Добавлена проверка при сохранении html-файла

--- templates/footer.php
+++ templates/footer.php
@@ -10,9 +10,9 @@
 		$html = ob_get_contents();
 		ob_end_clean();
 		$filename = str_replace('.php', '.html', basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
-		file_put_contents($filename, $html);
 
 		header('Content-Type: text/html; charset=utf-8');
-		echo '<h1>'.$filename.' сохранён.';
+		if (file_put_contents($filename, $html)) echo '<h1>'.$filename.' сохранён.</h1>';
+		else echo '<h1 style="color:red">Не удалось сохранить '.$filename.'</h1>';
 	}
 ?>
