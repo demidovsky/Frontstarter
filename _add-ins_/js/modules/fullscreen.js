6324d129c4eb8df6d242c8ac26748fc60247a545
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-03-31 18:29:39 +0300

    Рефакторинг

--- _add-ins_/js/modules/fullscreen.js
+++ _add-ins_/js/modules/fullscreen.js
@@ -2,27 +2,23 @@ $(function()
 {
 	"use strict";
 
-
-
 	// обработка события с отсечкой по таймауту
 	$.fn.onTimeout=function(e,t,n){var r=null,i=function(){if(r)clearTimeout(r);r=setTimeout(t,n)};return $(this).on(e,i)}
 
-
+	var $window = $(window);
 
 	// подстройка по высоте экрана
 	(function FullScreen()
 	{
-		var $window = $(window);
+		var minHeight = 0, // px
+			$full = $('.js-fullscreen').css({"margin":0, "padding":0}),
+			onResize = function()
+			{
+				console.log("onResize");
+				$full.height($window.height() > minHeight ? $window.height() : minHeight);
+			};
 
-		var minHeight = 0; // px
-		var $full = $('.js-fullscreen').css({"margin":0, "padding":0});
-		var onResize = function()
-		{
-			console.log("onResize");
-			$full.height($window.height() > minHeight ? $window.height() : minHeight);
-		};
 		onResize();
-
 		$window.onTimeout("resize", onResize, 100);
 	})();
 
