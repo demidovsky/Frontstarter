07eddf8736f9bbfb07c41b767b20b9d557922150
Dmitry Demidovsky <d.demidovsky@gmail.com>
2015-03-31 19:57:39 +0300

    Изменена работа со стилями и скриптами

--- _blank_/templates/footer.php
+++ _blank_/templates/footer.php
@@ -1,12 +1,20 @@
+		</div><!-- .b-content -->
+	</div><!-- .b-header-and-content -->
+
+
+	<footer class="b-footer">
+		© 2015
+	</footer>
 
 
 	<script src="js/libs/jquery-1.10.2.min.js"></script>
-	<script src="js/libs/modernizr.all.min.js"></script>
+	<? if (IS_BOOTSTRAP) { echo '<script src="bootstrap/js/bootstrap.min.js"></script>'; } ?>
 
-	<?
-		antiCache('js/script.js');
-		antiCache('js/modules/resize.js');
-	?>
+
+<?
+	antiCache('js/script.js');
+	antiCache('js/modules/resize.js');
+?>
 
 
 
@@ -15,14 +23,5 @@
 
 <?
 	// для сохранения html-файла
-	if (isset($_GET['html']) || SAVE_HTML)
-	{
-		$html = ob_get_contents();
-		ob_end_clean();
-		$filename = str_replace('.php', '.html', basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
-
-		header('Content-Type: text/html; charset=utf-8');
-		if (file_put_contents($filename, $html)) echo '<h1>'.$filename.' сохранён.</h1>';
-		else echo '<h1 style="color:red">Не удалось сохранить '.$filename.'</h1>';
-	}
+	if (isset($_GET['html']) || SAVE_HTML) saveHTML();
 ?>
