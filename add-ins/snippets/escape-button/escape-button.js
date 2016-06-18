27114852139b6150f3b10967b3c6600544820f6b
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-06-18 23:10:59 +0300

    Добавлено демо escape-button

--- add-ins/snippets/escape-button/escape-button.js
+++ add-ins/snippets/escape-button/escape-button.js
@@ -1,10 +1,28 @@
-$.fn.escape = function (callback) {
-    return this.each(function () {
-        $(document).on("keypress", this, function (e) {
-            var keycode = (e.keyCode ? e.keyCode : e.which);
-            if (keycode === 27) {
-                callback.call(this, e);
-            };
-        });
-    });
-};
+$(function()
+{
+	/*$.fn.escape = function(callback)
+	{
+		return this.each(function()
+		{
+			$(document).on("keypress", this, function(e)
+			{
+				var keycode = (e.keyCode ? e.keyCode : e.which);
+				if (keycode === 27)
+				{
+					callback.call(this, e);
+				};
+			});
+		});
+	};*/
+
+
+	$(document).on("keypress", this, function(e)
+	{
+		var keycode = (e.keyCode ? e.keyCode : e.which);
+		if (keycode === 27)
+		{
+			$(window).trigger("escape");
+		};
+	});
+
+});
