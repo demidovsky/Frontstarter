85b017f80c3dbdd549a4b1f3e8f120cdc1ac0c9d
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-06-19 11:52:39 +0300

    поправил отступы

--- add-ins/snippets/radio-deselect/radio-deselect.js
+++ add-ins/snippets/radio-deselect/radio-deselect.js
@@ -2,32 +2,36 @@ $(function()
 {
 
 
-$('[data-deselectable]').on("click", function(event)
-{
-	var $this = $(this);
 
-	console.log('click');
-
-	if ($this.data("clicked"))
+	// второй клик по радио отменяет выбор
+	$('[data-deselectable]').on("click", function(event)
 	{
-		console.log("deselect");
-		$this.data("clicked", false);
-		$this[0].checked = false;
-		$this
-			.attr('data-deselectable', false);
-	}
-	else
-	{
-		console.log("select");
-		$('input[name=' + $this[0].name + ']')
-			.data("clicked", false)
-			.attr('data-deselectable', false);
+		var $this = $(this);
 
-		$this
-			.data("clicked", true)
-			.attr('data-deselectable', true);
-	}
+		console.log('click');
+
+		if ($this.data("clicked"))
+		{
+			console.log("deselect");
+			$this.data("clicked", false);
+			$this[0].checked = false;
+			$this
+				.attr('data-deselectable', false);
+		}
+		else
+		{
+			console.log("select");
+			$('input[name=' + $this[0].name + ']')
+				.data("clicked", false)
+				.attr('data-deselectable', false);
+
+			$this
+				.data("clicked", true)
+				.attr('data-deselectable', true);
+		}
+
+	});
+
+
 
 });
-
-})
