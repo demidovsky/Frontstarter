8a20bec00d19e4ad1e570eb3fa14ba32bec92d9b
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-06-22 09:51:19 +0300

    добавлена min-версия для assert

--- add-ins/snippets/assert/assert.js
+++ add-ins/snippets/assert/assert.js
@@ -7,3 +7,5 @@ function assert(condition, message)
 	}
 }
 
+// min:
+// /*assert*/;function assert(condition,message){if(!condition){message=message||"Assert failed";throw(typeof Error!="undefined"?new Error(message):message)}};
