6c67f11f2d99653fc1f0f59810b12dde66cc9847
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-06-22 09:50:35 +0300

    демо countdown

--- add-ins/snippets/countdown/countdown.js
+++ add-ins/snippets/countdown/countdown.js
@@ -1,70 +1,66 @@
-var Countdown = 
+$(function()
 {
-	date: new Date(2016, 06, 16, 20, 0, 0),
-	timer: null,
-	values: { day:null, hour:null, min:null, sec:null },
-	elem: {
-		$wrapper: $('.countdown-wrapper'),
-		$day:  $('#countdown_day'),
-		$hour: $('#countdown_hour'),
-		$min:  $('#countdown_min'),
-		$sec:  $('#countdown_sec'),
-		$tick: $('#countdown_tick')
-	},
+	var Countdown = 
+	{
+		date: new Date(2016,8-1,5,	23,59,59), // месяцы начинаются с нуля
+		timer: null,
+		values: { day:null, hour:null, min:null, sec:null },
+		elem: {
+			$day:  $('#countdown_day'),
+			$hour: $('#countdown_hour'),
+			$min:  $('#countdown_min'),
+			$sec:  $('#countdown_sec'),
+			$tick: $('#countdown_tick')
+		},
 
-	update: function() {
-		var now = new Date(),
-			total = Math.floor((this.date.getTime() - now.getTime()) / 1000), // всего осталось секунд
-			sec = total % 60,
-			min = Math.floor(total / 60) % 60,
-			hour = Math.floor(total / 3600) % 24,
-			day = Math.floor(total / 3600 / 24);
+		update: function()
+		{
+			var now = new Date(),
+				total = Math.floor((this.date.getTime() - now.getTime()) / 1000), // всего осталось секунд
+				sec = total % 60,
+				min = Math.floor(total / 60) % 60,
+				hour = Math.floor(total / 3600) % 24,
+				day = Math.floor(total / 3600 / 24);
 
-		// время премьеры
-		if (total < 0 && total > -14400) // можно ограничить, например, 4 часами от начала
-		{
-			this.stop();
-			// дождались!
-			// ...
-		}
-		else
-		{
+			// время вышло
 			if (total < 0)
 			{
 				day = hour = min = sec = 0;
 				this.stop();
 			}
+
 			if (this.values.day != day)   { this.values.day = day;   this.elem.$day.html(day);  }
 			if (this.values.hour != hour) { this.values.hour = hour; this.elem.$hour.html(hour); }
 			if (this.values.min != min)   { this.values.min = min;   this.elem.$min.html(this.padZero(min));  }
 			if (this.values.sec != sec)   { this.values.sec = sec;   this.elem.$sec.html(this.padZero(sec));  }
-			
-			$('.countdown').hide();
+		},
+
+		start: function()
+		{
+			var self = this;
+
+			clearInterval(this.timer);
+			this.timer = setInterval(function(){ self.update(); }, 1000);
+			this.update();
+
+			this.elem.$tick.addClass('_tick');
+		},
+
+		stop: function()
+		{
+			clearInterval(this.timer);
+			this.elem.$tick.removeClass('_tick');
+
+			var self = this;
+			setTimeout(function() { self.elem.$tick.removeClass('_tick'); }, 100);
+		},
+
+		padZero: function(value)
+		{
+			return (value > 9 ? value : '0' + value);
 		}
-	},
+	};
 
-	start: function()
-	{
-		var self = this;
+	Countdown.start();
 
-		clearInterval(this.timer);
-		this.timer = setInterval(function(){ self.update(); }, 1000);
-		this.update();
-
-		this.elem.$tick.addClass('_tick');
-	},
-
-	stop: function()
-	{
-		clearInterval(this.timer);
-		this.elem.$tick.removeClass('_tick');
-
-		var self = this;
-		setTimeout(function() { self.elem.$tick.removeClass('_tick'); }, 100);
-	},
-
-	padZero: function(value)
-	{
-		return (value > 9 ? value : '0' + value);
-	}
-};
+})
