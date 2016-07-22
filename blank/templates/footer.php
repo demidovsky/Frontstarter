01f0fe7e6ffd0974374d83f6da58169f75d26769
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-07-22 06:45:05 +0300

    Переписана проверка старых браузеров

--- blank/templates/footer.php
+++ blank/templates/footer.php
@@ -2,10 +2,6 @@
 	<noscript>
 		<strong>В вашем браузере выключен javascript!</strong> Функциональность сайта может быть нарушена.
 	</noscript>
-	<div id="old_browser" style="display:none">
-		<b>x</b> <strong>Вы используете устаревший браузер!</strong> Функциональность сайта может быть нарушена. <br>
-		<small>Браузер не поддерживает: <span id="old_browser_bugs"></span></small>
-	</div>
 
 <?
 	// подключение скриптов:
