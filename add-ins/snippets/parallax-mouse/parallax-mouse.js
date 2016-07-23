c2b0855b0fce2dd7b3b5309122ab39463c846b41
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-07-23 15:24:27 +0300

    Демо parallax-mouse

--- add-ins/snippets/parallax-mouse/parallax-mouse.js
+++ add-ins/snippets/parallax-mouse/parallax-mouse.js
@@ -1,33 +1,44 @@
-;(function Parallax()
+$(function()
 {
-	var $prx = $('.js-parallax-bg');
 
-	if ($prx.length)
+
+
+	function initParallax()
 	{
-		$prx.each(function(index, element)
+		var $prx = $('[data-parallax]');
+
+		if ($prx.length)
 		{
-			var SHIFT = 0.25,
-				$this = $(element),
-				thisX = $this.offset().left,
-				thisY = $this.offset().top,
-				w = $this.width(),
-				h = $this.height(),
-				wShift = w * SHIFT,
-				hShift = h * SHIFT;
-
-			$this.css("background-size", (w + wShift) + "px", (h + hShift) + "px");
-
-			$this.unbind("mousemove").on("mousemove", function(event)
+			$prx.each(function(index, element)
 			{
-				var x = event.pageX - thisX,
-					y = event.pageY - thisY;
+				var SHIFT = 0.05,
+					$this = $(element),
+					thisX = $this.offset().left,
+					thisY = $this.offset().top,
+					w = $this.width(),
+					h = $this.height(),
+					wShift = w * SHIFT,
+					hShift = h * SHIFT;
 
-				console.log(wShift * x/w, hShift * y/h);
+				$this.css("background-size", (w>h?(w+wShift)+"px":"auto") + " " + (w<h?(h+hShift)+"px":"auto"));
 
-				$this.css("background-position", (-wShift * x/w) + "px " + (-hShift * y/h) + "px");
+				$this.off("mousemove").on("mousemove", function(event)
+				{
+					var x = event.pageX - thisX,
+						y = event.pageY - thisY;
 
-			})
-		});
+					$this.css("background-position", (-wShift * x/w) + "px " + (-hShift * y/h) + "px");
+				});
+			});
+		}	
 	}
 
-})();
+
+	// handles the last event over time
+	$.fn.over=function(e,c,t){var r=null,a=this,s=function(event){if(r)clearTimeout(r);r=setTimeout(function(){c.call(a,event)},t)};return $(this).on(e,s)};
+	
+	$(window).over("resize", function() {console.log('!');initParallax();}, 100);
+	initParallax();
+
+
+});
