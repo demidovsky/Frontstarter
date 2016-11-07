bb866365b66172ab64721d67b6b391d3acf548d5
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-11-07 15:39:57 +0300

    Демо owl-carousel

---
+++ add-ins/snippets/owl-carousel/init-owlcarousel.js
@@ -0,0 +1,22 @@
+$(function owlCarousel()
+{
+
+	$('.owl-carousel').each(function(index, element)
+	{
+		var $this = $(element),
+			items = parseInt($this.attr("data-owlcarousel-items"), 10);
+
+		if (isNaN(items)) items = 1;
+
+		$this.owlCarousel(
+		{
+			items: items,
+			nav: false,
+			dots: true,
+			loop: true,
+			// autoWidth: true,
+			navRewind: false
+		});
+	});
+
+});
