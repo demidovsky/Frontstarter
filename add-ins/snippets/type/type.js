7f34caaff547a359ab9e2f639331afe730857352
Dmitry Demidovsky <d.demidovsky@gmail.com>
2016-11-17 12:30:49 +0300

    Улучшено демо type

--- add-ins/snippets/type/type.js
+++ add-ins/snippets/type/type.js
@@ -1,3 +1,26 @@
+/**
+ * Продвинутая проверка типов данных
+ * @see _helpers/assert.js
+ * @see http://www.slideshare.net/yandex/writing-maintainable-javascript-26754863
+ * @memberOf module:_helpers
+ * @param {*} value - проверяемая переменная.
+ * @returns {String} тип переменной
+ * @example type(1)             = "int"
+ * @example type(0.2)           = "float"
+ * @example type(NaN)           = "NaN"
+ * @example type(1/0)           = "infinity"
+ * @example type("test")        = "string"
+ * @example type([])            = "array"
+ * @example type([1,2,3])       = "array"
+ * @example type({})            = "object"
+ * @example type({a:1})         = "object"
+ * @example type(true)          = "boolean"
+ * @example type(null)          = "null"
+ * @example type(undefined)     = "undefined"
+ * @example type(new Date())    = "date"
+ * @example type(function(){})  = "function"
+ * @example type(document.body) = "dom"
+ */
 function type(value)
 {
 	var typeName = Object.prototype.toString.call(value).match(/\[object (.*?)\]/)[1].toLowerCase();
